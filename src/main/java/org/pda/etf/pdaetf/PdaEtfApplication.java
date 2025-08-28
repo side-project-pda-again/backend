44c27e4d088757b1ecc9014ee910bac21e32a3bd
Han_Yeong_Jang <qpwisu@kookmin.ac.kr>
2025-08-28 15:43:39 +0900

    Init backend

---
+++ src/main/java/org/pda/etf/pdaetf/PdaEtfApplication.java
@@ -0,0 +1,13 @@
+package org.pda.etf.pdaetf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdaEtfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdaEtfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/pda/etf/pdaetf/PdaEtfApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pda.etf.pdaetf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdaEtfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
